d59cf48f64b6965f5221a5c26ca34b8e547b92e8
a0uda <abdullah.oudaa@gmail.com>
2024-05-08 18:24:42 +0300

    backend

---
+++ AptBe/AptBe/src/main/java/com/backend/AptBe/AptBeApplication.java
@@ -0,0 +1,22 @@
+package com.backend.AptBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AptBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptBeApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+
+}
---
+++ AptBe/AptBe/src/test/java/com/backend/AptBe/AptBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.AptBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
