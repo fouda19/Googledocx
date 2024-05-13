0780e36658890d88231bd1793092369733e64d19
a0uda <abdullah.oudaa@gmail.com>
2024-05-13 12:15:31 +0300

    a

--- Socket/src/main/java/com/Apt/Socket/Socket.java
+++ Socket/src/main/java/com/Apt/Socket/Socket.java
@@ -12,14 +12,28 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import org.springframework.web.client.RestTemplate;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Socket extends TextWebSocketHandler {
     private static Map<String, Set<WebSocketSession>> docSessionsMap = new HashMap<>();
     private static Map<String, String> docContentMap = new HashMap<>();
+    private static Map<String,Integer> docCounterMap = new HashMap<>();
+    private static Map<String, List<ArrayList<?>>> docChangesMap = new HashMap<>();
 
     @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) {
+    public synchronized void handleTextMessage(WebSocketSession session, TextMessage message) {
         // implement your message handling logic here
+        String mess = message.getPayload();
+        System.out.println("Message: " + mess);
+        String documentId = (String) session.getAttributes().get("documentId");
+        System.out.println("Document ID: " + documentId);
+        String sessionId = session.getId();
+        System.out.println("Session ID: " + sessionId);
+
+        ArrayList<WebSocketSession> sessions = new ArrayList<>(docSessionsMap.get(documentId));
+        System.out.println("Sessions: " + sessions);
+
         System.out.println("Message received");
     }
 
@@ -42,10 +56,15 @@ public class Socket extends TextWebSocketHandler {
             String url = "http://192.168.100.4:3003/backend/documents/doc/" + documentId;
             response = restTemplate.getForObject(url, String.class);
             docContentMap.put(documentId, response);
+            docCounterMap.put(documentId, 0);
+            docChangesMap.put(documentId, new ArrayList<>());
         }
         String content = docContentMap.get(documentId);
-        String message = "{\"type\": \"init\", \"content\": \"" + content + "\"}";
-        session.sendMessage(new TextMessage(message));
+        if (content == null) {
+            content = "";
+        }
+        Integer counter = docCounterMap.get(documentId);
+        String message = "{\"type\": \"init\", \"content\": \"" + content + "\", \"counter\": \"" + counter + "\"}";        session.sendMessage(new TextMessage(message));
 
         docSessionsMap.get(documentId).add(session);
         System.out.println("Document Sessions: " + docSessionsMap.get(documentId).size());
@@ -59,10 +78,26 @@ public class Socket extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         System.out.println("Connection closed");
         String documentId = (String) session.getAttributes().get("documentId");
-        docSessionsMap.get(documentId).remove(session);
-        System.out.println("Document Sessions: " + docSessionsMap.get(documentId).size());
+        Set<WebSocketSession> sessions = docSessionsMap.get(documentId);
+
+        Integer sizer = 0;
+        System.out.println("Document ID: " + documentId);
+        System.out.println("Session ID: " + session.getId());
         System.out.println("Document Sessions: " + docSessionsMap.get(documentId));
-        if (docSessionsMap.get(documentId).size() == 0) {
+        System.out.println(sessions);
+
+        if (sessions != null) {
+            sizer = sessions.size();
+            docSessionsMap.get(documentId).remove(session);
+            System.out.println("Document Sessions: " + docSessionsMap.get(documentId).size());
+            System.out.println("Document Sessions: " + docSessionsMap.get(documentId));
+        }
+        else
+        {
+            System.out.println("Document Sessions: 0");
+        }
+        
+        if (sizer - 1 <= 0) {
             RestTemplate restTemplate = new RestTemplate();
             String url = "http://192.168.100.4:3003/backend/documents/saveDoc/" + documentId;
             String content = docContentMap.get(documentId);
--- frontend/aptFrontend/src/components/Editor.jsx
+++ frontend/aptFrontend/src/components/Editor.jsx
@@ -1,82 +1,267 @@
-import React, { useEffect, useState } from "react";
-import { useMutation, useQuery } from "react-query";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import { fetchRequest, postRequest } from "../API/API";
+import React, { useState, useRef, useEffect } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css"; // Import Quill styles
 import { useParams } from "react-router-dom";
 let WebSock;
+let oldContent = "";
+let counter = 0;
+let changesQueue = [];
+let editor;
+let docContent = "";
 const Editor = () => {
-  const [editorHtml, setEditorHtml] = useState("");
+  const quillRef = useRef(null);
   const { documentId } = useParams();
-
-  // let WebSock = new WebSock(`ws://192.168.100.4:4004/editor/${documentId}`);
-  // let webSocket; // Declare the WebSocket object outside the useEffect hook
-
+  const [docContent, setDocContent] = useState("");
   useEffect(() => {
-    configureWebSock(); // Open the WebSocket connection
-    // return () => {
-    //   WebSock.close(); // Close the WebSocket connection
-    // };
-  }, []);
+    setupWebSocket();
+    if (!quillRef.current) {
+      editor = new Quill("#editor-container", {
+        theme: "snow",
+        placeholder: "Write something...",
+      });
+      quillRef.current = editor;
+    }
+    if (docContent) {
+      console.log("DOC", docContent);
+      editor.root.innerHTML = docContent;
+    }
+    
+    if (quillRef.current) {
+      const handleChange = function (delta, oldDelta, source) {
+        console.log("DELTA", delta.ops);
+        let isDelete = false;
+        let isInsert = false;
+        let isBold = false;
+        let isItalic = false;
+        let currentIndex = 0;
+        delta.ops.forEach((operation) => {
+          if (operation.delete) {
+            isDelete = true;
+          }
+          if (operation.retain && !operation.attributes) {
+            currentIndex += operation.retain;
+          }
+          if (operation.insert) {
+            isInsert = true;
+          }
+          if (
+            operation.attributes &&
+            operation.attributes.hasOwnProperty("bold")
+          ) {
+            const boldValue = operation.attributes.bold;
+            if (boldValue == true) {
+              isBold = true;
+            } else if (boldValue == false) {
+              isBold = false;
+            } else {
+              isBold = null;
+            }
+          }
+          if (
+            operation.attributes &&
+            operation.attributes.hasOwnProperty("italic")
+          ) {
+            const val = operation.attributes.italic;
+            if (val == true) {
+              isItalic = true;
+            } else if (val == false) {
+              isItalic = false;
+            } else {
+              isItalic = null;
+            }
+          }
+        });
+        if (source == "user" && source != "api") {
+          handleTextChange(isDelete, isBold, isItalic, isInsert, currentIndex);
+          oldContent = quillRef.current.root.innerHTML;
+        }
+      };
 
-  const configureWebSock = () => {
+      quillRef.current.on("text-change", handleChange);
+      return () => {
+        if (quillRef.current) {
+          quillRef.current.off("text-change", handleChange);
+        }
+        if (WebSock && WebSock.readyState === WebSocket.OPEN) {
+          WebSock.close();
+          WebSock = null;
+        }
+      };
+    }
+
+    oldContent = "";
+    return () => {
+      if (WebSock && WebSock.readyState === WebSocket.OPEN) {
+        WebSock.close();
+      }
+      if (editor) {
+        editor.off("text-change");
+        // editor.removeAllListeners();
+        editor.destroy();
+      }
+    };
+  }, [docContent]);
+  function setupWebSocket() {
+    console.log("Setting up websocket");
     const url = `ws://192.168.100.4:3001/editor/${documentId}`;
     WebSock = !WebSock ? new WebSocket(url) : WebSock;
-    WebSock.onopen = () => {
-      console.log("Websocket connected");
+
+    WebSock.onopen = function () {
+      console.log("WebSocket connection established");
     };
-    WebSock.onmessage = (event) => {
-      console.log(event.data, "event.data");
+    WebSock.onmessage = function (event) {
       console.log("Websocket message received");
       let JsonData = JSON.parse(event.data);
       console.log(JsonData, "JsonData");
       if (JsonData.type == "init") {
-        setEditorHtml(JsonData.content);
+        // editor.root.innerHTML = JsonData.content;
+        // if (quillRef.current != null) {
+        //   quillRef.current.root.innerHTML = JsonData.content;
+        // }
+        // editor.root.innerHTML = JsonData.content;
+        // quillRef.current = editor;
+        setDocContent(JsonData.content);
+        console.log("DOCzzzzzzz", docContent);
+        console.log("JSON V ABL", counter);
+        counter = JsonData.counter;
+        console.log("JSON V B3d", counter);
+      } else if (JsonData.type == "ack") {
+        changesQueue = changesQueue.filter((change) => {
+          return !(
+            change.index == JsonData.index &&
+            change.character == JsonData.character &&
+            change.isDelete == JsonData.isDelete &&
+            change.isBold == JsonData.isBold &&
+            change.isItalic == JsonData.isItalic &&
+            change.isInsert == JsonData.isInsert
+          );
+        });
       } else {
-        setEditorHtml(editorHtml + event.data); //CHECK
+        changesQueue.forEach(function (change) {
+          if (data.index > change.index) {
+            data.index = data.index + 1;
+          }
+        });
+        modifyCharInText(
+          data.index,
+          data.character,
+          data.isDelete,
+          data.isBold,
+          data.isItalic,
+          data.isInsert
+        );
       }
+      // const data = JSON.parse(event.data);
+      // console.log("recieved", data);
+      counter++;
+      oldContent = quillRef.current.root.innerHTML;
     };
-    WebSock.onclose = (event) => {
-      console.log("Websocket closed", event);
-    };
-    WebSock.onerror = (error) => {
-      console.log("WebSocket error: ", error);
-    };
-  };
-  // WebSock.onopen = () => {
-  //   console.log("WebSock connected");
-  // };
-  // const { data, isLoading, isError } = useQuery(
-  //   ["editor", documentId],
-  //   () => {
-  //     fetchRequest(`/backend/documents/${documentId}`);
-  //   },
-  //   {
-  //     onSuccess: (data) => {
-  //       setEditorHtml(data.text);
-  //     },
-  //   }
-  // );
-  // const postReq = useMutation(postRequest);
 
-  // const handleEditorChange = (html) => {
-  //   console.log(html, "html");
-  //   setEditorHtml(html);
-  //   postReq.mutate(`/backend/documents/${documentId}`, { text: html });
-  // };
+    WebSock.onclose = function (event) {
+      console.log("WebSocket connection closed");
+    };
+  }
+
+  function modifyCharInText(
+    index,
+    character,
+    isDelete,
+    isBold,
+    isItalic,
+    isInsert
+  ) {
+    console.log("HII", isBold);
+
+    if (isBold == null) {
+      quillRef.current.formatText(index, 1, "bold", false);
+    } else if (isItalic == null) {
+      quillRef.current.formatText(index, 1, "italic", false);
+    } else if (isBold && !isInsert) {
+      quillRef.current.formatText(index, 1, "bold", true);
+    } else if (isItalic && !isInsert) {
+      quillRef.current.formatText(index, 1, "italic", true);
+    } else {
+      let text = quillRef.current.root.innerHTML.replace(/<\/?[^>]+(>|$)/g, "");
+      let length = text.length;
+      let formatsArray = [];
+      for (let i = 0; i < length; i++) {
+        formatsArray.push({
+          char: text[i],
+          format: quillRef.current.getFormat(i, 1),
+        });
+      }
+      if (isDelete) {
+        text = text.slice(0, index) + text.slice(index + 1);
+        formatsArray.splice(index, 1);
+      } else {
+        text = text.slice(0, index) + character + text.slice(index);
+        formatsArray.splice(index, 0, {
+          format: { italic: isItalic, bold: isBold },
+        });
+      }
+      quillRef.current.setText(text, "api");
+      length = text.length;
+      for (let i = 0; i < length; i++) {
+        quillRef.current.formatText(i, 1, formatsArray[i].format);
+      }
+    }
+
+    // Set the cursor position to the end of the editor
+    quillRef.current.setSelection(index + 1);
+  }
+
+  function findChanges(isDelete, isBold, isItalic, isInsert, currentIndex) {
+    // const quill = quillRef.current.getEditor();
+    const newContent = quillRef.current.root.innerHTML;
+    const strippedOldContent = oldContent.replace(/<\/?[^>]+(>|$)/g, ""); //mal
+    const strippedNewContent = newContent.replace(/<\/?[^>]+(>|$)/g, ""); //ml
+    let character = strippedNewContent[currentIndex]; //abc
+    if (currentIndex >= strippedNewContent.length)
+      character = strippedOldContent[currentIndex];
+    let changes = [];
+    changes.push({
+      index: currentIndex,
+      character,
+      isDelete,
+      isAck: false,
+      isBold,
+      isItalic,
+      isInsert,
+      counter,
+    });
+    oldContent = quillRef.current.root.innerHTML;
+    return changes;
+  }
+
+  function handleTextChange(
+    isDelete,
+    isBold,
+    isItalic,
+    isInsert,
+    currentIndex
+  ) {
+    const changes = findChanges(
+      isDelete,
+      isBold,
+      isItalic,
+      isInsert,
+      currentIndex
+    );
+    if (changes.length > 0) {
+      console.log("changes", changes);
+      if (WebSock.readyState === WebSocket.OPEN) {
+        WebSock.send(JSON.stringify(changes[0]));
+      } else {
+        console.error("WebSocket connection is not open.");
+      }
+
+      changesQueue.push(changes[0]);
+    }
+  }
 
   return (
     <>
-      <div className="flex flex-col h-[calc(94vh-var(--navbar-height))]">
-        <div className="w-full sm:w-full md:w-full lg:w-full xl:w-full h-[calc(94vh-var(--navbar-height))]">
-          <ReactQuill
-            theme="snow"
-            value={editorHtml}
-            className="w-full mb-4 h-[calc(94vh-var(--navbar-height))]"
-            placeholder="Text (optional) "
-          />
-        </div>
-      </div>
+      <div className="flex flex-col mx-10 w-full" id="editor-container"></div>
     </>
   );
 };
